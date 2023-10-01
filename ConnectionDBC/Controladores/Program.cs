589af5cb686d4c1e84d709429212d9b13c382008
Jose Manuel <csi23-jmorrra@colegioaltair.net>
2023-10-01 02:33:01 +0200

    Primer Commit

---
+++ ConnectionDBC/Controladores/Program.cs
@@ -0,0 +1,9 @@
+﻿using Npgsql;
+using System.Configuration;
+public class program 
+{
+    public static void main(string[] args) 
+    {
+        
+    }
+}
---
+++ ConnectionDBC/Servicios/connection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace ConnectionDBC.Servicios
+{
+    internal class connection
+    {
+        public NpgsqlConnection generarConexion() 
+        {
+            string stringConexionPostgresql = ConfigurationManager.ConnectionStrings["stringConexion"].ConnectionString;
+            Console.WriteLine("[INFORMACIÓN-ConexionPostgresqlImplementacion-generarConexionPostgresql] Cadena conexión: " + stringConexionPostgresql);
+            NpgsqlConnection conn = null;
+            string estado = "";
+
+            if (!string.IsNullOrWhiteSpace(stringConexionPostgresql)) {
+                try
+                {
+                    conn = new NpgsqlConnection(stringConexionPostgresql);
+                    conn.Open();
+                    //Se obtiene el estado de conexión para informarlo por consola
+                    estado = conn.State.ToString();
+                    if (estado.Equals("Open"))
+                    {
+
+                        Console.WriteLine("[INFORMACIÓN-ConexionPostgresqlImplementacion-generarConexionPostgresql] Estado conexión: " + estado);
+
+                    }
+                    else
+                    {
+                        conn = null;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("[ERROR-ConexionPostgresqlImplementacion-generarConexionPostgresql] Error al generar la conexión:" + e);
+                    conn = null;
+                    return conn;
+                }
+                    return conn;
+            }
+        }
+    }
+}
