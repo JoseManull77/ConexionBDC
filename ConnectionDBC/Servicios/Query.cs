92e5de275662dc7b1fef8d9aa7cfcd9b33bcb191
Jose Manuel <csi23-jmorrra@colegioaltair.net>
2023-10-03 00:02:55 +0200

    Creacion de los metodos de Query

--- ConnectionDBC/Servicios/Query.cs
+++ ConnectionDBC/Servicios/Query.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NpgsqlTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace ConnectionDBC.Servicios
 {
     internal class Query
     {
+        public void insertLibro( string titulo, string autor, string isbn, int edicion) 
+        { 
+            
+        }
+        public NpgsqlTsQuery selectLibro(long idLibro)
+        {
+
+        }
+        public void updateLibro() 
+        { 
+            
+        }
+        public void deleteLibro(long idLibro) 
+        {
+            
+        }
     }
 }
